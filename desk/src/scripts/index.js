6e404dbfec53f5656b0abbd28176418015fc1ede
exslym <76844562+exslym@users.noreply.github.com>
2022-02-03 10:10:35 +0300

    appearing animations fixed

--- desk/src/scripts/index.js
+++ desk/src/scripts/index.js
@@ -8,8 +8,12 @@ window.addEventListener('DOMContentLoaded', function () {
 	'use strict';
 
 	const pos = document.documentElement;
+	const circle = document.querySelector('.circle');
+	const box = document.querySelectorAll('.box')[1];
 	pos.addEventListener('mousemove', e => {
 		pos.style.setProperty('--x', e.clientX + 'px');
 		pos.style.setProperty('--y', e.clientY + 'px');
+		circle.classList.add('appearing');
+		box.classList.add('appearing');
 	});
 });
